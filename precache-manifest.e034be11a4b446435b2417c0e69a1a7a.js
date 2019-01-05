d7aa6d8b9db7b9f915d2eab26bb40cc535de4de7
imajoriri <imajo.srroni@gmail.com>
2019-01-05 15:57:45 +0900

    Updates

---
+++ precache-manifest.e034be11a4b446435b2417c0e69a1a7a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5abccab6757f68c50b06",
+    "url": "/memo-front/static/js/runtime~main.5abccab6.js"
+  },
+  {
+    "revision": "8102b01a59e6d8e39e72",
+    "url": "/memo-front/static/js/main.8102b01a.chunk.js"
+  },
+  {
+    "revision": "321bb26e30479f291dfc",
+    "url": "/memo-front/static/js/1.321bb26e.chunk.js"
+  },
+  {
+    "revision": "c0012861ff66a6804ebd8b63e1038174",
+    "url": "/memo-front/index.html"
+  }
+];
